3b94fa46d5a2c9aba6b853be309da3f63561be9a
761695784 <malcom70976@gmail.com>
2024-06-03 23:17:05 +0000

    fonctionnalite modifier réussie

--- MarnaBarro/routes/web.php
+++ MarnaBarro/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BienController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/liste', [ BienController:: class,'liste' ]);
+
+Route::get('/modifier_bien/{id}', [BienController::class, 'modifier_bien']);
+
+Route::POST('/modifier/traitement', [BienController::class, 'modifier_traitement']);
